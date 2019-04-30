5d6a4b3bb021d761f217e849381f40fc078b8223
yustikasaridirgahayu <48982094+yustikasaridirgahayu@users.noreply.github.com>
2019-05-01 04:29:32 +0800

    Create kesimpulanLINKEDlist.cpp

---
+++ kesimpulanLINKEDlist.cpp
@@ -0,0 +1,9 @@
+SINGLE linked list adalah sekumpulan dari node yang saling terhubung dengan node lain melalui sebuah ponter.rangkaian single 
+linked list terssebut diawali dengan sebuah huad untuk menyimpan alamat awal dan di akhiri dengan node yang mengarah pointer ke null.
+DOUBLE linked list adalah sekumpulan node data yang terurut liner data yang terurut linear linear atau sekuensial dengan dua buah 
+pointer yaitu prev dan next.
+Operasi pada Double Linked List
+
+Double linked list memiliki beberapa operasi dasar pada list, misalnya penyisipan, penghapusan, menampilkan maju, dan menampilkan mundur.
+
+sirkular memiliki pointer next yang akan menunjuk diri sehingga berputar, sedangkan linear tidaK berputar 
